fd5bea3b006b8b1ab9a7a5f2357e37d269aca313
Petra-78 <partos.petra.0305@gmail.com>
2025-04-24 16:24:10 +0200

    random picture picker code shortened

--- text.js
+++ text.js
@@ -1,42 +1,11 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
 
-function pickDice1 () {
-if (randomNumber1 === 1) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
-} else if (randomNumber1 === 2) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
-} else if (randomNumber1 === 3) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
-} else if (randomNumber1 === 4) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
-} else if (randomNumber1 === 5) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
-} else {
-    document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
-}
-}
 
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
 
-function pickDice2 () {
-if (randomNumber2 === 1) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
-} else if (randomNumber2 === 2) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
-} else if (randomNumber2 === 3) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
-} else if (randomNumber2 === 4) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-} else if (randomNumber2 === 5) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
-} else {
-    document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
-}
-}
-
-pickDice1();
-pickDice2();
 
 function winner() {
 if (randomNumber1 > randomNumber2) {
