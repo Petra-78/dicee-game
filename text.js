5e7a7acb28a975619837c38ea75ca9049b9cc78c
Petra-78 <partos.petra.0305@gmail.com>
2025-04-24 15:55:11 +0200

    winner text added

--- text.js
+++ text.js
@@ -22,19 +22,30 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 function pickDice2 () {
 if (randomNumber2 === 1) {
     document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
-} else if (randomNumber1 === 2) {
+} else if (randomNumber2 === 2) {
     document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
-} else if (randomNumber1 === 3) {
+} else if (randomNumber2 === 3) {
     document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
-} else if (randomNumber1 === 4) {
+} else if (randomNumber2 === 4) {
     document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-} else if (randomNumber1 === 5) {
+} else if (randomNumber2 === 5) {
     document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
 } else {
     document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
 }
 }
 
+pickDice1();
+pickDice2();
 
-pickDice1()
-pickDice2()
+function winner() {
+if (randomNumber1 > randomNumber2) {
+     document.querySelector("h1").textContent = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").textContent = "It's a draw!"; 
+}
+}
+
+winner();
